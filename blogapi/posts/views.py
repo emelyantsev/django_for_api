8dd3c85f23d2754f89da399bc011ae87bce1267c
emelyantsev <aleks.twin@gmail.com>
2020-10-20 11:38:50 +0300

    add user detail and listview using viewsets and routers

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,9 +1,13 @@
-from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+#from rest_framework import generics, permissions
+
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly 
 
+from rest_framework import viewsets
 
+""" 
 class PostList(generics.ListCreateAPIView):
     #permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
@@ -14,3 +18,22 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer """
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
